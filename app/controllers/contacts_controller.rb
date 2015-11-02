42afa719b57513281a547adaf199016cb1f03f33
wbucko <bucko.wojciech@gmail.com>
2015-11-02 15:51:52 +0000

    Added mailer to contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,12 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            message = params[:contact][:message]
+            
+            ContactMailer.contact_email(name, email, message).deliver
+            
             flash[:success] = 'Wiadomość wysłano.'
             redirect_to new_contact_path
         else
---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+   default to 'bucko.wojciech@gmail.com'
+   
+   def contact_email
+      @name = name
+      @email = email
+      @message = message
+      
+      mail(from: email, subject: 'eDziennik - Wiadomość z formularza kontaktowego')
+   end
+end
