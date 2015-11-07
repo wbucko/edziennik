8f008d0cc202b9a11951871f53e3ca1b6f75067b
wbucko <bucko.wojciech@gmail.com>
2015-11-07 12:17:55 +0000

    Fixed some styles and link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
     
     def home
+        if user_signed_in?
+            redirect_to user_path(current_user)
+        end
     end
     
     def about
