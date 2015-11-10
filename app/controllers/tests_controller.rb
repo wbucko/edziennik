e6b130b39343b1b015c49dffbacbcadef925aad9
wbucko <bucko.wojciech@gmail.com>
2015-11-10 06:17:19 +0000

    Added form for new test

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,22 @@
+class TestsController < ApplicationController
+   
+   def new
+       @test = Test.new
+   end
+   
+   def create
+       
+   end
+   
+   def edit
+       
+   end
+   
+   def update
+       
+   end
+   
+   def destroy
+       
+   end
+end
