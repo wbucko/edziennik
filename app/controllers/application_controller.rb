77a6263d3dacf8eaa2edec3ec8d55e7fd9c9f8f0
wbucko <bucko.wojciech@gmail.com>
2015-11-12 15:56:00 +0000

    Added come copy to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 def after_sign_in_path_for(resource)
-  if current_user.profile
-    user_path(current_user)
+  if current_user.email == 'admin@admin.pl'
+    root_path
   else
-    new_user_profile_path(current_user)
+    if current_user.profile
+      user_path(current_user)
+    else
+      new_user_profile_path(current_user)
+    end
   end
 end
 
 def admin_verify
   if current_user
-   redirect_to user_path(current_user) unless current_user.email == 'admin@admin.pl'
+  redirect_to user_path(current_user) unless current_user.email == 'admin@admin.pl'
   else
-   redirect_to root_path
+  redirect_to root_path
   end
 end
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     
     def home
-        if user_signed_in?
+        if current_user.email != 'admin@admin.pl'
             redirect_to user_path(current_user)
         end
     end
